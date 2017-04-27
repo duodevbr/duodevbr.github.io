5f5593078ab3b0345935380f120dd2f3b395b1d7
rafaelv <eu@rafaelv.com.br>
2017-04-27 20:57:38 -0300

    update conteudo / organizacao folders e pub

--- assets/js/main.js
+++ assets/js/main.js
@@ -327,32 +327,32 @@ var settings = {
 		// Off-Canvas Navigation.
 
 			// Navigation Panel Toggle.
-				$('<a href="#navPanel" class="navPanelToggle"></a>')
-					.appendTo($header);
+				// $('<a href="#navPanel" class="navPanelToggle"></a>')
+				// 	.appendTo($header);
 
 			// Navigation Panel.
-				$(
-					'<div id="navPanel">' +
-						'<nav>' +
-							$('#nav').navList() +
-						'</nav>' +
-						'<a href="#navPanel" class="close"></a>' +
-					'</div>'
-				)
-					.appendTo($body)
-					.panel({
-						delay: 500,
-						hideOnClick: true,
-						hideOnSwipe: true,
-						resetScroll: true,
-						resetForms: true,
-						side: 'right'
-					});
+				// $(
+				// 	'<div id="navPanel">' +
+				// 		'<nav>' +
+				// 			$('#nav').navList() +
+				// 		'</nav>' +
+				// 		'<a href="#navPanel" class="close"></a>' +
+				// 	'</div>'
+				// )
+				// 	.appendTo($body)
+				// 	.panel({
+				// 		delay: 500,
+				// 		hideOnClick: true,
+				// 		hideOnSwipe: true,
+				// 		resetScroll: true,
+				// 		resetForms: true,
+				// 		side: 'right'
+				// 	});
 
 			// Fix: Remove transitions on WP<10 (poor/buggy performance).
-				if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
-					$('#navPanel')
-						.css('transition', 'none');
+				// if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
+				// 	$('#navPanel')
+				// 		.css('transition', 'none');
 
 	});
 
